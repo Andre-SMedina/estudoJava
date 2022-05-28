9712223137f6f33aa90fa01ce22a22f34b483b99
André Luís Silva Medina <106404962+Andre-SMedina@users.noreply.github.com>
2022-05-28 18:11:39 -0300

    identação

--- src/threads/Sync.java
+++ src/threads/Sync.java
@@ -1,70 +1,70 @@
 package threads;
 
 //Use a synchronized block to control access to SumArray.  
-class SumArray {  
-private int sum;  
+class SumArray {
+	private int sum;
 
-int sumArray(int nums[]) {  
- sum = 0; // reset sum  
+	int sumArray(int nums[]) {
+		sum = 0; // reset sum
 
- for(int i=0; i<nums.length; i++) {  
-   sum += nums[i];  
-   System.out.println("Running total for " +  
-          Thread.currentThread().getName() +  
-          " is " + sum);  
-   try {  
-     Thread.sleep(10); // allow task-switch  
-   }  
-   catch(InterruptedException exc) {  
-     System.out.println("Thread interrupted.");  
-   }  
- }  
- return sum; 
-}  
-}   
+		for (int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+			System.out.println("Running total for " + Thread.currentThread().getName() + " is " + sum);
+			try {
+				Thread.sleep(10); // allow task-switch
+			} catch (InterruptedException exc) {
+				System.out.println("Thread interrupted.");
+			}
+		}
+		return sum;
+	}
+}
 
-class MyThread implements Runnable {  
-Thread thrd;  
-static SumArray sa = new SumArray();  
-int a[];  
-int answer; 
+class MyThread implements Runnable {
+	Thread thrd;
+	static SumArray sa = new SumArray();
+	int a[];
+	int answer;
 
 // Construct a new thread.  
-MyThread(String name, int nums[]) {  
- thrd = new Thread(this, name);  
- a = nums;  
- thrd.start(); // start the thread  
-}  
+	MyThread(String name, int nums[]) {
+		thrd = new Thread(this, name);
+		a = nums;
+		thrd.start(); // start the thread
+	}
 
 // Begin execution of new thread.  
-public void run() {  
- int sum;  
+	public void run() {
+		int sum;
 
- System.out.println(thrd.getName() + " starting.");  
+		System.out.println(thrd.getName() + " starting.");
 
- // synchronize calls to sumArray()  
- synchronized(sa) {  
-   answer = sa.sumArray(a);           
- }  
- System.out.println("Sum for " + thrd.getName() +  
-                    " is " + answer);  
+		// synchronize calls to sumArray()
+		synchronized (sa) {
+			answer = sa.sumArray(a);
+		}
+		System.out.println("Sum for " + thrd.getName() + " is " + answer);
 
- System.out.println(thrd.getName() + " terminating.");  
-}  
-}  
+		System.out.println(thrd.getName() + " terminating.");
+	}
+}
 
-class Sync {  
-public static void main(String args[]) {  
- int a[] = {1, 2, 3, 4, 5};  
+class Sync {
+	public static void main(String args[]) {
+		int a[] = { 1, 2, 3, 4, 5 };
 
- MyThread mt1 = new MyThread("Child #1", a);  
- MyThread mt2 = new MyThread("Child #2", a);  
+		MyThread mt1 = new MyThread("Child #1", a);
+		MyThread mt2 = new MyThread("Child #2", a);
+		MyThread mt3 = new MyThread("Child #3", a);
+		MyThread mt4 = new MyThread("Child #4", a);
 
- try {  
-   mt1.thrd.join();  
-   mt2.thrd.join();  
- } catch(InterruptedException exc) {  
-   System.out.println("Main thread interrupted.");  
- }  
-}  
-} 
+		try {
+			mt1.thrd.join();
+			mt2.thrd.join();
+			mt3.thrd.join();
+			mt4.thrd.join();
+		} catch (InterruptedException exc) {
+			System.out.println("Main thread interrupted.");
+		}
+	}
+}
