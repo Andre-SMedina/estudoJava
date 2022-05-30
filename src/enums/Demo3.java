83e4fe680aedc441bf68b3c06ec1c67a6056029e
André Luís Silva Medina <106404962+Andre-SMedina@users.noreply.github.com>
2022-05-30 12:09:39 -0300

    Update Demo3.java

--- src/enums/Demo3.java
+++ src/enums/Demo3.java
@@ -2,30 +2,38 @@ package enums;
 
 //Use an enum constructor, instance variable, and method.  
 enum Transport {
-	CAR(65), TRUCK(55), AIRPLANE(600), TRAIN(70), BOAT(22);
+	CAR(65, 100), TRUCK(55, 600), AIRPLANE(600, 900 ), TRAIN(70, 2500), BOAT(22, 40 );
 
-	private int speed; // typical speed of each transport
+	private int speed, load; // typical speed of each transport
 
 // Constructor  
-	Transport(int s) {
+	Transport(int s, int c) {
 		speed = s;
+		load = c;
 	}
 
 	int getSpeed() {
 		return speed;
 	}
+	
+	int getLoad() {
+		return load;
+	}
 }
 
 class Demo3 {
 	public static void main(String args[]) {
-		Transport tp;
+		//Transport tp;
 
 		// Display speed of an airplane.
 		System.out.println("Typical speed for an airplane is " + Transport.AIRPLANE.getSpeed() + " miles per hour.\n");
 
 		// Display all Transports and speeds.
 		System.out.println("All Transport speeds: ");
-		for (Transport t : Transport.values())
-			System.out.println(t + " typical speed is " + t.getSpeed() + " miles per hour.");
+		for (Transport t : Transport.values()) {
+			System.out.println(t + " typical speed is " + t.getSpeed() + " miles per hour. ");
+			System.out.println("And load is " + t.getLoad() + " Kg\n");
+		}
+		for(Transport t : Transport.values()) System.out.print(t + " posição " + t.ordinal() + ", ");
 	}
 }
